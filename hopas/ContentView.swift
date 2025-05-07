4468e42b4acac0f0859418abc92d9b5b23d78f0e
Connor Link <contact@connorjlink.com>
2025-05-06 20:27:07 -0500

    Initial Commit

---
+++ hopas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hopas
+//
+//  Created by Connor Link on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hopas/hopasApp.swift
@@ -0,0 +1,17 @@
+//
+//  hopasApp.swift
+//  hopas
+//
+//  Created by Connor Link on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct hopasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
